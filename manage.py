9397c737a52595f0c09d90bebc0f45e4a79aaa0f
m <mmailm@mail.ru>
2015-07-08 00:38:23 +0800

    add migrations support, add scss styles

---
+++ manage.py
@@ -0,0 +1,11 @@
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
+from app import app, db
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
